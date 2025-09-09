2ba5b98e543548c3e7d355d14072b14ad01dc4a2
James-9696 <hujm969696@163.com>
2025-09-09 02:09:07 -0700

    fix: cannot click after component reference, modify checkbox style

--- packages/demos/app/radio/event.vue
+++ packages/demos/app/radio/event.vue
@@ -4,15 +4,23 @@
     <tiny-radio v-model="value" label="1" @change="changeAction">男</tiny-radio>
     <tiny-radio v-model="value" label="2" @change="changeAction">女</tiny-radio>
     <p>{{ text }}</p>
+    <br/> <br/>
+    <tiny-checkbox-group v-model="checked">
+      <tiny-checkbox label="复选框1">复选框1</tiny-checkbox>
+      <tiny-checkbox label="复选框2">复选框2</tiny-checkbox>
+    </tiny-checkbox-group>
+    <p>当前选中的值为：{{ checked }}</p>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import { TinyRadio } from '@opentiny/vue-mobile'
+import { TinyRadio, TinyCheckbox, TinyCheckboxGroup } from '@opentiny/vue-mobile'
 
 const value = ref('1')
 const text = ref('男')
+const checked = ref(['复选框1'])
+
 
 function changeAction(value) { // NISVUE3 FIXME: value重复定义，请手工修改
   text.value = value === '1' ? '男' : '女'
